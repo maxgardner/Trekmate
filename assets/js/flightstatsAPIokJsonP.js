39c5dbe1b42b6c77900389598066946b2b63311b
Roman Brito <Roman Brito>
2017-03-17 21:39:15 -0500

    roman changes

--- assets/js/flightstatsAPIokJsonP.js
+++ assets/js/flightstatsAPIokJsonP.js
@@ -28,3 +28,50 @@ $("#add-flight").on("click", function(event) {
     addAPIscript("flightstatus", "rest", "v2", "jsonp", parameters, "", "flightInfo");
 
 });
+
+// add flight modal
+$('document').ready(function() {
+// // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+
+});
+
+var date = null;
+var res = null;
+var year = null;
+var month = null;
+var day = null;
+var airlineName = null;
+var flightNumber = null;
+
+// airline name
+var airlineName = $("#autocomplete-input").val();
+
+$("#flight-info").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    date = $("#flightDate").val();
+    res = date.split("-");
+    year = res[0];
+    month = res[1];
+    day = res[2];
+
+    // airline name
+    airlineName = $("#autocomplete-input").val();
+    flightNumber = $("#flightNumber").val();
+
+    var flightKey = database.ref("trips/" + tripId + "/flights").push().key;
+    database.ref("trips/" + tripId + "/flights" + flightKey).set({
+        year: year,
+        month,
+        day,
+        airlineName,
+        flightNumber
+    });
+
+});
+
+// // datepicker
+// $('.datepicker').pickadate({
+//
+// });
